930d6f44259247c2881d518a96264f06f9b96c5d
Lushogv <luciano20746@gmail.com>
2022-12-29 02:11:41 -0300

    delete cartSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from '../features/cart/cartSlice'
 import userReducer from '../features/user/userSlice'
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer,
     user: userReducer
   }
 });
